f02fbe58b008ed2e823277a939b99141d11eb513
Devansh Jani <79559686+djman323@users.noreply.github.com>
2025-11-10 01:10:58 +0530

    edit : UI edits

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // static export friendly
   output: 'export',
+  // Optimization settings
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  // Trailing slashes for static export
+  trailingSlash: true,
+  // Disable unnecessary features for static export
+  experimental: {
+    // This disables server components as they're not needed for static export
+    appDir: false,
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // Set dark theme as default
+    document.documentElement.setAttribute('data-theme', 'dark')
+  }, [])
+
   return (
     <Layout>
       <Component {...pageProps} />
